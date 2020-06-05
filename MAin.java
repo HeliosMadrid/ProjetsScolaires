84272bcbe89c80ef5a571c95b1e227b269b2b532
Helios_38 <elliot.madrid38@gmail.com>
2020-06-05 09:49:50 +0200

    calcul en +

--- MAin.java
+++ MAin.java
@@ -2,8 +2,16 @@ package;
 
 public class Main
 {
+	private int result;
+	
 	public static void main(String[] args)
 	{
-
-	}System.out.println("test");
+		System.out.println("test");
+		
+		if(args.lenght >= 1)
+		{
+			this.result = Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+			System.out.println(result);
+		}
+	}
 }
